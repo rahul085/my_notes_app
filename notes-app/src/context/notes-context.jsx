ab45c6188a1596469f82de92bc6f68eb3a7a7fe2
Rahul <rahulneeli2003@gmail.com>
2026-02-23 12:46:08 +0530

    saved to local storage

--- notes-app/src/context/notes-context.jsx
+++ notes-app/src/context/notes-context.jsx
@@ -1,24 +1,54 @@
-import { createContext,useContext,useReducer } from "react";
+import { createContext,useContext,useEffect,useReducer } from "react";
 import { NotesReducer } from "../reducers/NotesReducer";
 
 const NotesContext=createContext();
 
 const NotesProvider=({children})=>{
-     const initialState = {
-    title: "",
-    text: "",
-    notes: [],
-    archive:[],
-    bin:[],
-    important:[],
-    isEditing:false,
-    editNoteId: null,
-   
+  const getInitialState=()=>{
+    const savedData=localStorage.getItem("my_notes_app");
+    if(savedData){
+      return JSON.parse(savedData);
+    }
+    return{
+      title: "",
+      text: "",
+      notes: [],
+      archive: [],
+      bin: [],
+      important: [],
+      isEditing: false,
+      editNoteId: null,
+
+    };
   };
-  const [{ title, text, notes,archive,bin,important,isEditing,editNoteId}, notesDispatch] = useReducer(
-    NotesReducer,
-    initialState,
-  );
+  //    const initialState = {
+  //   title: "",
+  //   text: "",
+  //   notes: [],
+  //   archive:[],
+  //   bin:[],
+  //   important:[],
+  //   isEditing:false,
+  //   editNoteId: null,
+   
+  // };
+
+  // const [{ title, text, notes,archive,bin,important,isEditing,editNoteId}, notesDispatch] = useReducer(
+  //   NotesReducer,
+  //   getInitialState()
+  // );
+
+  const [state,notesDispatch]=useReducer(NotesReducer,getInitialState());
+
+  //Whenever  'state' changes, save it to Local storage 
+  useEffect(()=>{
+    localStorage.setItem("my_notes_app",JSON.stringify(state));
+  },[state])
+
+  const { title, text, notes,archive,bin,important,isEditing,editNoteId}=state;
+
+
+
 
     return(
         <NotesContext.Provider value={{ title, text, notes,archive,bin,important,isEditing,editNoteId,notesDispatch }}>
